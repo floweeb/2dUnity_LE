146454d7e0a861f539d48e2348306ad4c47ec826
floweeb <floydqaranja@gmail.com>
2024-08-09 19:13:42 +0300

    Basic left/right movement and jumping though has an infite jump so basically can fly

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private int speed = 5;
     private Rigidbody2D body;
+    
     // Awake is called when the script instance is being loaded
     private void Awake()
     {
@@ -10,6 +12,8 @@ public class PlayerMovement : MonoBehaviour
     }
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal"), body.velocity.y);
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        if(Input.GetKey(KeyCode.Space) )
+            body.velocity = new Vector2(body.velocity.x, speed);
     }
 }
