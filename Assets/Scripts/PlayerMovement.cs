8ae80a10a689b27599d9e379e59a55ff835a701b
floweeb <floydqaranja@gmail.com>
2024-08-09 18:32:36 +0300

    First commit, though a bunch is already done in this project

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+    // Awake is called when the script instance is being loaded
+    private void Awake()
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
