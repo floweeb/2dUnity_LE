8b4ead87c2f1f6a4fc45973b6ef2f3595de9e109
floweeb <floydqaranja@gmail.com>
2024-08-10 16:24:00 +0300

    Basic animations done, now started on the script part

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,14 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private int speed = 5;
     private Rigidbody2D body;
+    private Animator anim;
     
     // Awake is called when the script instance is being loaded
     private void Awake()
     {
+        // Help grab references for RigidBody2D and Animator from game object
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
     private void Update()
     {
