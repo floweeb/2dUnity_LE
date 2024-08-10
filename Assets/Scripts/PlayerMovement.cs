3f6bb4ad922ebce096a4283c2b0e26109944c355
floweeb <floydqaranja@gmail.com>
2024-08-10 16:53:02 +0300

    Added the jump mechanics, and solved the infinite jump, 2D collision and tagging is introduced here

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private int speed = 5;
     private Rigidbody2D body;
     private Animator anim;
+    private Boolean grounded;
     
     // Awake is called when the script instance is being loaded
     private void Awake()
@@ -25,10 +26,22 @@ public class PlayerMovement : MonoBehaviour
         else if (horizontal_input < -0.01f)
             transform.localScale = new Vector3(-1.5f, 1.5f, 1.5f);
 
-        if(Input.GetKey(KeyCode.Space) )
-            body.velocity = new Vector2(body.velocity.x, speed);
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
 
         // Set animator parameters
         anim.SetBool("run", horizontal_input != 0);
+        anim.SetBool("grounded", grounded);
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        grounded = false;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
     }
 }
